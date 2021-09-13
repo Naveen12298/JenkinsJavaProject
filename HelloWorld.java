c26215e54b9d8682d40b8acaafb4acb649a79f53
Naveen12298 <naveenkumarrofficial@gmail.com>
2021-09-13 10:52:56 +0530

    added java code for jenkins

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld{
+    public static void main(String[] s){
+        System.out.println("HEllO IBM");
+        for(int i=0; i<=10;i++){
+            System.out.println("value of I :" + i);
+        }
+    }
+
+}
